220d416de1c2dbd93c1706c4566d87d7c15259f8
Pann Phyu <pannphyu24497@gmail.com>
2025-01-22 15:16:43 +0630

    adding new features for API testing

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # restful module can be used for both odoo16_enterprise and odoo16_community versions
-# my testing api for employee data in Odoo's HR Employee module
+# test api for employee details in Odoo's HR Employee module
--- employee_data/controllers/hr_employee.py
+++ employee_data/controllers/hr_employee.py
@@ -11,7 +11,7 @@ from odoo.addons.restful.controllers.main import (
 
 class LoadingEmployee(http.Controller):
     @validate_token
-    @http.route("/api/employee-list", type="http", auth="none", methods=["GET"], csrf=False)
+    @http.route("/api/employee-lists", type="http", auth="none", methods=["GET"], csrf=False)
     def get_employee_list(self,page_no=None, per_page=None):
         """
         return list of employee
@@ -28,9 +28,51 @@ class LoadingEmployee(http.Controller):
             values.append({
                 'id': rec.id,
                 'name': rec.name,
-                'mobile_phone': rec.mobile_phone
+                'mobile_phone': rec.mobile_phone,
+                'department': {
+                    'id': rec.department_id.id,
+                    'name': rec.department_id.name
+                },
             })
         start_index = (int(page_no) - 1) * int(per_page)
         end_index = start_index + int(per_page)
         response_values['result'] = values[start_index:end_index]
         return valid_response(response_values)
+
+    @validate_token
+    @http.route("/api/employee-lists/<id>", type="http", auth="none", methods=["GET"], csrf=False)
+    def get_dedicated_employee_detail(self, id=None):
+        """
+        return employee detail
+        """
+        try:
+            id = int(id)
+        except ValueError:
+            return valid_response({"status": "fail", "message": "Id must be an integer"})
+
+        detail = request.env['hr.employee'].sudo().browse(int(id))
+        if not detail:
+            return valid_response({"status": "fail", "message": "Employee is not found!"})
+
+        data = {
+                'id': detail.id,
+                'name': detail.name,
+                'mobile_phone': detail.mobile_phone,
+                'work_phone': detail.work_phone,
+                'work_email': detail.work_email,
+                'work_address': {
+                    'id': detail.address_id.id,
+                    'name': detail.address_id.name
+                },
+                'department': {
+                    'id': detail.department_id.id,
+                    'name': detail.department_id.name
+                },
+                'manager': {
+                    'id': detail.parent_id.id,
+                    'name': detail.parent_id.name
+                },
+                'marital': detail.marital,
+            }
+
+        return valid_response(data)
